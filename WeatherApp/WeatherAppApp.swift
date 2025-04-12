405350c35ac554454c57425e02b4288e8bbb5975
Nermeen Tomoum <nrmeen.tomoum@alumni.fci-cu.edu.eg>
2025-04-12 10:40:13 +0200

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Nermeen Tomoum on 12/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
