1385328dd0c60d712e9efc72157df510f0897c8d
Nermeen Tomoum <nrmeen.tomoum@alumni.fci-cu.edu.eg>
2025-04-12 12:49:26 +0200

    create UI for weather screen based on weatherType
    create: subviews
    DailyWeatherRow
    TempretureView

--- WeatherApp/Models/WeatherTypes.swift
+++ WeatherApp/Models/WeatherTypes.swift
@@ -9,6 +9,40 @@ import Foundation
 
 enum WeatherType: String, Codable {
     case sunny
-    case claudy
+    case cloudy
     case rain
+}
+
+extension WeatherType {
+    var name: String {
+        switch self {
+        case .sunny:
+            return "SUNNY"
+        case .cloudy:
+            return "CLOUDY"
+        case .rain:
+            return "RAINY"
+        }
+    }
+    var iconName: String {
+        switch self {
+        case .sunny:
+            return "sunny"
+        case .cloudy:
+            return "cloudy"
+        case .rain:
+            return "rainy"
+        }
+    }
+    
+    var backgroundpicture: String {
+        switch self {
+        case .sunny:
+            return "forest_sunny"
+        case .cloudy:
+            return "forest_cloudy"
+        case .rain:
+            return "forest_rainy"
+        }
+    }
 }
